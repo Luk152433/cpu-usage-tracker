54e0d2c63cc1f23b17754eff8f1fd52eeca7e2e7
Lukasz Krzyskow <lukaszkrzyskow9@gmail.com>
2023-06-06 00:35:58 +0200

    ignore file5

---
+++ build/src/CMakeFiles/Functions.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Functions.dir/analyzer.c.o"
+  "CMakeFiles/Functions.dir/analyzer.c.o.d"
+  "CMakeFiles/Functions.dir/buffer.c.o"
+  "CMakeFiles/Functions.dir/buffer.c.o.d"
+  "CMakeFiles/Functions.dir/printer.c.o"
+  "CMakeFiles/Functions.dir/printer.c.o.d"
+  "CMakeFiles/Functions.dir/reader.c.o"
+  "CMakeFiles/Functions.dir/reader.c.o.d"
+  "CMakeFiles/Functions.dir/thread.c.o"
+  "CMakeFiles/Functions.dir/thread.c.o.d"
+  "libFunctions.a"
+  "libFunctions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Functions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
